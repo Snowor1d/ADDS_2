9c4a91133c453b62a49bf4cafcdd4e9541d3f2b5
snowor1d <437travel@naver.com>
2025-01-08 13:06:30 +0900

    3.1.1 -> mode에 direction dependency 반영

--- sitl_v3.1/ADDS_AS_reinforcement.py
+++ sitl_v3.1/ADDS_AS_reinforcement.py
@@ -216,7 +216,7 @@ class HybridPolicyNetwork(nn.Module):#행동을 샘플링하고 정책 학습, �
 
         # Combine action
         action = torch.cat([direction, mode_one_hot], dim=1)  # (B,4)
-
+        print("action : ", action)
         # Combine log-probs
         log_prob = log_prob_cont + log_prob_mode  # (B,)
 
@@ -244,8 +244,9 @@ class HybridSACAgent:
 
         # For optional epsilon exploration (if you want it):
         self.epsilon = start_epsilon
-        self.epsilon_min = 0.01
+        self.epsilon_min = 0.1
         self.epsilon_decay = 0.95
+        self.direction = [0, 0]
 
         # Replay buffer
         self.replay_buffer = ReplayBuffer(capacity=replay_size)
@@ -321,7 +322,7 @@ class HybridSACAgent:
         with torch.no_grad():
             action_t, _ = self.policy.sample_action(state_t)
         action = action_t.cpu().numpy()[0]  # shape (4,)
-        print(action)
+
         if deterministic:
             # You could return mean + argmax for the mode 
             # (requires rewriting sample_action). 
