0f8d555960b3913667a41fbc6c272e8135ebf754
snowor1d <437travel@naver.com>
2025-12-14 22:16:26 +0900

    REWARD_L 추가

--- SAC_ME/ADDS_AS_reinforcement.py
+++ SAC_ME/ADDS_AS_reinforcement.py
@@ -403,8 +403,10 @@ def worker_process(
                         r_i = env_model.reward_based_alived_root() * REWARD_I
                     if REWARD_J:
                         r_j = env_model.reward_based_all_agents_danger_root() * REWARD_J
+                    if REWARD_L:
+                        r_l = env_model.reward_based_farthest_agent_distance() * REWARD_L
 
-                    reward += (r_a + r_b + r_c + r_d + r_e + r_g + r_h + r_i + r_j + r_k + REWARD_FIXED)
+                    reward += (r_a + r_b + r_c + r_d + r_e + r_g + r_h + r_i + r_j + r_k + r_l + REWARD_FIXED)
                     r_k = 0.0
 
                     # -----------------------------
