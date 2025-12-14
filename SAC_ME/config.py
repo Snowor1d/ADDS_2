0ca6e31a532963e98853d2107978d92ce0f33bd3
snowor1d <437travel@naver.com>
2025-12-14 13:32:05 +0900

    wrong parameter correction

--- SAC_ME/config.py
+++ SAC_ME/config.py
@@ -4,7 +4,7 @@ BUFFER_SIZE = 1000000
 BATCH_SIZE = 128
 INTRINSIC_ETA = 0.1 #intrinsic reward
 START_BATCH_TIMES = 1
-START_UPDATE_EPISODE = 10
+START_UPDATE_EPISODE = 500
 DEVICE = "cpu"
 
 GAMMA_START = 0.99
@@ -18,7 +18,7 @@ CROWD_NUMBER_MIN = 50
 CROWD_NUMBER_MAX = 50
 MAP_NUM = -1 #if not used, -2, if random, -1
 MAP_NUM_RANDOM = [6]
-SCALE_CHECK = 1 # want to check reward scale?
+SCALE_CHECK = 0 # want to check reward scale?
 ACTION_SCALE = 4
 MAX_STEPS = 4000
 
