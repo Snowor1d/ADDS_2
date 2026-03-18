1cb4473221b1ef53e8e215c12266718b591f095f
snowor1d <437travel@naver.com>
2026-03-18 16:23:47 +0900

    attention7 : spatial attention per robot agent + film with ego info

--- SAC_MARL_Attention7/config.py
+++ SAC_MARL_Attention7/config.py
@@ -1,5 +1,5 @@
 # Soft Actor Critic for Multi-Agent Reinforcement Learning with 
-# Local Reward, Impala CNN, Attention for fused state (global+ego+auxiliary state), individual Q valued, film
+# Local Reward, Impala CNN, Attention for fused state (global+ego+auxiliary state), individual Q valued, film with ego info, spatial attention
 # ------------- BASIC PARAMETERS ------------------
 LR = 1e-4
 BUFFER_SIZE = 1000000
@@ -83,10 +83,10 @@ RANDOM_EXIT = False
 
 
 # -------------- PATH -------------------
-LOG_DIR = "Log_MARL_Attention6_105_108_128"
+LOG_DIR = "Log_MARL_Attention7_105_108_128"
 #LOG_DIR = "SOTA_MODELS"
 #LOG_DIR = "Log_test"
-PORT_NUM = 7626
+PORT_NUM = 7787
 
 # --------------- SAC ALGORITHM PARAMETER ---------------
 LOG_STD_MAX = 0.5
