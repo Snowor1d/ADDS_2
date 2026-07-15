44ca22d807e77c0b06476aacb0dd85154eec1758
snowor1d <437travel@naver.com>
2026-07-15 18:09:24 +0900

    file arrangement

--- DREAMER_MARL2_L/dreamer_v3/__init__.py
+++
@@ -1,5 +0,0 @@
-from .agent import DreamerAgent
-from .config import DreamerConfig
-from .replay import DreamerSequenceReplay
-
-__all__ = ["DreamerAgent", "DreamerConfig", "DreamerSequenceReplay"]
--- DREAMER_MARL7_s/__Init__.py
+++
@@ -1,9 +0,0 @@
-from .core import Agent, Model, RandomActivation, DataCollector
-from .space import MultiGrid, ContinuousSpace
-from .visualize import Viewer
-
-
-__all__ = [
-"Agent", "Model", "RandomActivation", "DataCollector",
-"MultiGrid", "ContinuousSpace", "Viewer",
-]
